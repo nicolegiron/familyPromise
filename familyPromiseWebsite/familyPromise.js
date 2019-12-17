d85603e3073e500413ef5b97a678df7f4aac4459
nicolegiron1 <ngiron@ucvts.org>
2019-12-16 22:48:06 -0500

    maps inputted

--- familyPromiseWebsite/familyPromise.js
+++ familyPromiseWebsite/familyPromise.js
@@ -15,3 +15,22 @@ window.onclick = function(event) {
     }
   }
 }
+
+function openMap(evt, mapName) {
+
+  // Get all elements with class="tabcontent" and hide them
+  var tabcontent = document.getElementsByClassName("tabcontent");
+  for (var i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  var tablinks = document.getElementsByClassName("tablinks");
+  for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(mapName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
